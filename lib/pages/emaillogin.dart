8a50d9d67346809122d9253be4e5cda39539ecd8
Satyam Shrivastav <mysatyam99@gmail.com>
2023-10-27 00:44:33 +0530

    Files Uploaded

--- lib/pages/emaillogin.dart
+++ lib/pages/emaillogin.dart
@@ -26,47 +26,51 @@ class _EmailLoginPageState extends State<EmailLoginPage> {
                       fit: BoxFit.fill))),
           Padding(
             padding: const EdgeInsets.all(8.0),
-            child: ListView(
-              children: [
-                const SizedBox(
-                  height: 20,
-                ),
-                const Text(
-                  "Let's\nConnect",
-                  style: TextStyle(fontSize: 50, fontWeight: FontWeight.w500),
-                ),
-                
-                const Spacer(flex: 1),
-                const SizedBox(
-                  height: 40,
-                ),
-                InputText(
-                  false,
-                  hintText: "Enter Username",
-                  labelText: "Username",
-                  context: context,
-                ).inputTextField(),
-                const SizedBox(
-                  height: 40,
-                ),
-                InputText(
-                  false,
-                  hintText: "Enter Email",
-                  labelText: "Email",
-                  context: context,
-                ).inputTextField(),
-                const SizedBox(
-                  height: 40,
-                ),
-                InputText(true,
-                        hintText: "Enter Password",
-                        labelText: "Password",
-                        context: context)
-                    .inputTextField(),
-                    const SizedBox(height: 40,),
-                ElevatedButton(onPressed: () {}, child: const Text("Sign Up")),
-                const Spacer(flex: 1),
-              ],
+            child: Expanded(
+              child: ListView(
+                children: [
+                  const SizedBox(
+                    height: 20,
+                  ),
+                  const Text(
+                    "Let's\nConnect",
+                    style: TextStyle(fontSize: 50, fontWeight: FontWeight.w500),
+                  ),
+                  const Spacer(flex: 1),
+                  const SizedBox(
+                    height: 40,
+                  ),
+                  InputText(
+                    false,
+                    hintText: "Enter Username",
+                    labelText: "Username",
+                    context: context,
+                  ).inputTextField(),
+                  const SizedBox(
+                    height: 40,
+                  ),
+                  InputText(
+                    false,
+                    hintText: "Enter Email",
+                    labelText: "Email",
+                    context: context,
+                  ).inputTextField(),
+                  const SizedBox(
+                    height: 40,
+                  ),
+                  InputText(true,
+                          hintText: "Enter Password",
+                          labelText: "Password",
+                          context: context)
+                      .inputTextField(),
+                  const SizedBox(
+                    height: 40,
+                  ),
+                  ElevatedButton(
+                      onPressed: () {}, child: const Text("Sign Up")),
+                  const Spacer(flex: 1),
+                ],
+              ),
             ),
           ),
         ],
