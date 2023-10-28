b3a4991a91fd9abff87c5071ee5cb777c443f0e5
Satyam Shrivastav <mysatyam99@gmail.com>
2023-10-28 23:27:49 +0530

    till yet

---
+++ lib/authorization/storage.dart
@@ -0,0 +1,11 @@
+// import 'package:cloud_firestore/cloud_firestore.dart';
+
+// class Storage {
+//   FirebaseFirestore users = FirebaseFirestore.instance;
+//   Future getUserList() async {
+//     return await users.doc(uid).snapshots();
+//   }
+// }
+
+
+//! Till Auth app is done , till yet not even list of people is shown
--- lib/pages/homepage.dart
+++ lib/pages/homepage.dart
@@ -15,53 +15,70 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-        minimum: const EdgeInsets.only(left: 8, right: 8),
         child: Scaffold(
-          appBar: AppBar(
-            actions: [
-              Padding(
-                padding: const EdgeInsets.only(right: 10),
-                child: GestureDetector(
-                    onTap: () {
-                      showDialog(
-                          context: context,
-                          builder: (context) => AlertDialog(
-                                title: const Text(
-                                  "Do you really want to Log out",
-                                  style: TextStyle(color: Colors.red),
-                                ),
-                                actions: [
-                                  GestureDetector(
-                                      onTap: () => SignUpMethods(_auth).logOut(context),
-                                      child: const Text(
-                                        "Yes",
-                                        style: TextStyle(color: Colors.red),
-                                      )),
-                                  const SizedBox(
-                                    width: 20,
+            backgroundColor: backgroundColor,
+            appBar: AppBar(
+              actions: [
+                Padding(
+                  padding: const EdgeInsets.only(right: 10),
+                  child: GestureDetector(
+                      onTap: () {
+                        showDialog(
+                            context: context,
+                            builder: (context) => AlertDialog(
+                                  title: const Text(
+                                    "Do you really want to Log out",
+                                    style: TextStyle(color: Colors.red),
                                   ),
-                                  GestureDetector(onTap: () => Navigator.of(context).pop(),
-                                    child: const Text(
-                                      "No",
-                                      style: TextStyle(color: Colors.green),
+                                  actions: [
+                                    GestureDetector(
+                                        onTap: () => SignUpMethods(_auth)
+                                            .logOut(context),
+                                        child: const Text(
+                                          "Yes",
+                                          style: TextStyle(color: Colors.red),
+                                        )),
+                                    const SizedBox(
+                                      width: 20,
                                     ),
-                                  ),
-                                ],
-                              ));
-                    },
-                    child: const Icon(
-                      Icons.output_rounded,
-                      color: Colors.red,
-                      size: 30,
-                    )),
-              )
-            ],
-            title: Text(
-              "Chats",
-              style: TextStyle(color: Colors.cyan.shade100, fontSize: 25),
+                                    GestureDetector(
+                                      onTap: () => Navigator.of(context).pop(),
+                                      child: const Text(
+                                        "No",
+                                        style: TextStyle(color: Colors.green),
+                                      ),
+                                    ),
+                                  ],
+                                ));
+                      },
+                      child: const Icon(
+                        Icons.output_rounded,
+                        color: Colors.red,
+                        size: 30,
+                      )),
+                )
+              ],
+              title: Text(
+                "Chats",
+                style: TextStyle(color: Colors.cyan.shade100, fontSize: 25),
+              ),
+              backgroundColor: darkBackgroundColor,
             ),
-            backgroundColor: darkBackgroundColor,
-          ),
-        ));
+            body: const Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Column(
+                children: [
+                  SizedBox(
+                      width: double.infinity,
+                      height: 70,
+                      child: Card(
+                        child: Text(
+                          "the_extremity\nmysatyam99@gmail.com",
+                          style: TextStyle(color: Colors.black,fontSize: 20),
+                        ),
+                      ))
+                ],
+              ),
+            )));
   }
 }
